2985c761af94486ecd5d1b30283495b6a97e5864
Antonio Blazquez <joblazqu@gmail.com>
2025-06-25 11:45:25 +0200

    fix edge function

--- api/contact.js
+++ api/contact.js
@@ -1,9 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
